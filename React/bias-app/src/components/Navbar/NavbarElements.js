390419f2c65f3fadadc169971303b5130e9ec23b
Shra <shrayswarup@gmail.com>
2023-11-09 01:20:21 -0700

    UI Color Theme Update

--- React/bias-app/src/components/Navbar/NavbarElements.js
+++ React/bias-app/src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-background: #ffffff;
+background: #C1C1C1;
 height: 85px;
 display: flex;
 justify-content: space-between;
